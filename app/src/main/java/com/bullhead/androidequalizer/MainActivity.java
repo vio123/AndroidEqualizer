7388ceb54638e2cdedbd83112913a0d15cc2b7c5
vior123 <viorelsmart9@gmail.com>
2021-09-08 18:41:32 +0300

    Update

--- app/src/main/java/com/bullhead/androidequalizer/MainActivity.java
+++ app/src/main/java/com/bullhead/androidequalizer/MainActivity.java
@@ -37,23 +37,6 @@ public class MainActivity extends AppCompatActivity {
                 .replace(R.id.eqFrame, equalizerFragment)
                 .commit();
     }
-
-    private void showInDialog() {
-        int sessionId = mediaPlayer.getAudioSessionId();
-        if (sessionId > 0) {
-            DialogEqualizerFragment fragment = DialogEqualizerFragment.newBuilder()
-                    .setAudioSessionId(sessionId)
-                    .title(R.string.app_name)
-                    .themeColor(ContextCompat.getColor(this, R.color.primaryColor))
-                    .textColor(ContextCompat.getColor(this, R.color.textColor))
-                    .accentAlpha(ContextCompat.getColor(this, R.color.playingCardColor))
-                    .darkColor(ContextCompat.getColor(this, R.color.primaryDarkColor))
-                    .setAccentColor(ContextCompat.getColor(this, R.color.secondaryColor))
-                    .build();
-            fragment.show(getSupportFragmentManager(), "eq");
-        }
-    }
-
     @Override
     protected void onStop() {
         super.onStop();
